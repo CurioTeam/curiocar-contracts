7fcd57668c2739fdf82825106b2dbd2a9fbe206e
Vladimir Sushkov <volodya.sushkov@gmail.com>
2018-06-27 18:51:27 +0300

    Add init script (after migrate contracts), and drafts of abi export files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,8 @@ const CurioAuction = artifacts.require("./CurioAuction.sol"),
 const auctionFee = 500; // 5% auction fee (from 0 to 10 000)
 
 module.exports = function(deployer, network, accounts) {
-  const deployerAccount = accounts[0];
+  const ownerAccount = accounts[0];
 
-  deployer.deploy(Curio, { from: deployerAccount })
-    .then(() => deployer.deploy(CurioAuction, Curio.address, auctionFee, { from: deployerAccount }));
+  deployer.deploy(Curio, { from: ownerAccount })
+    .then(() => deployer.deploy(CurioAuction, Curio.address, auctionFee, { from: ownerAccount }));
 };
--- scripts/init.js
+++ scripts/init.js
@@ -1,14 +1,39 @@
-// TODO: Add init script after migrate contracts
-
-const util = require('./utils/util');
+const CurioAuction = artifacts.require("./CurioAuction.sol"),
+      Curio = artifacts.require("./Curio.sol");
 
 const init = async function (accounts) {
-  const deployerAccount = accounts[0];
+  const ownerAccount = accounts[0],
+        adminAccount = accounts[1];
 
   console.log("Init script start.");
 
   console.log("Accounts:");
-  console.log(deployerAccount + " (deployer)");
+  console.log(ownerAccount + " (owner)");
+  console.log(adminAccount + " (admin)");
+
+  let core = await Curio.deployed();
+  console.log("Core address: " + Curio.address);
+
+  let auction = await CurioAuction.deployed();
+  console.log("Auction address: " + CurioAuction.address);
+
+  console.log("Setting auction address..");
+  await core.setAuctionAddress(CurioAuction.address, { from: ownerAccount });
+
+  let auctionAddress = await core.auction();
+  console.log("Auction address: " + auctionAddress);
+
+  console.log("Setting core admin address..");
+  await core.setAdmin(adminAccount, { from: ownerAccount });
+
+  let coreAdmin = await core.adminAddress();
+  console.log("Core admin address: " + coreAdmin);
+
+  console.log("Unpausing core contract..");
+  await core.unpause({ from: ownerAccount });
+
+  let isCorePaused = await core.paused();
+  console.log("Core paused: " + isCorePaused);
 
   console.log("Init script end.");
 };
